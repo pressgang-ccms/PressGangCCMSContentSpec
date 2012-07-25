a2d592f302e02663627ea76cb4fbbbc644957459
lnewson <lnewson@localhost>
2012-07-25 18:42:16 +1000

    Added missing Rendering Message classes

---
+++ src/main/java/org/jboss/pressgangccms/docbook/compiling/DocbookBuildType.java
@@ -0,0 +1,11 @@
+package org.jboss.pressgangccms.docbook.compiling;
+
+/**
+ * This class defines the entity types that can be rendered by the Docbook Renderer service
+ */
+public final class DocbookBuildType
+{
+	public static final int TOPIC = 0;
+	public static final int TRANSLATEDTOPIC = 1;
+	public static final int CONTENTSPECIFICATON = 2;
+}
---
+++ src/main/java/org/jboss/pressgangccms/docbook/compiling/DocbookRendererMessage.java
@@ -0,0 +1,22 @@
+package org.jboss.pressgangccms.docbook.compiling;
+
+/**
+ * This class defines the message that is sent to the Docbook Renderer Service
+ * which specifies what kind of entity is being rendered, and the entity id.
+ * This will be converted to JSON when sent to the messaging service.
+ */
+public class DocbookRendererMessage
+{
+	public int entityType;
+	public int entityId;
+	
+	public DocbookRendererMessage()
+	{
+	}
+	
+	public DocbookRendererMessage(final int entityId, final int entityType)
+	{
+		this.entityType = entityType;
+		this.entityId = entityId;
+	}
+}

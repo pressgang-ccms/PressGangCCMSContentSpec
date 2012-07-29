112ac6591611bfe2c4cc3b48cd233d2be06eadd8
Matthew Casperson <mcaspers@redhat.com>
2012-07-30 07:37:18 +1000

    Switched to Java 1.6

---
+++ src/main/java/org/jboss/pressgangccms/docbook/messaging/TopicRendererType.java
@@ -0,0 +1,11 @@
+package org.jboss.pressgangccms.docbook.messaging;
+
+/**
+* This class defines the entity types that can be rendered by the Docbook Renderer service
+*/
+public final class TopicRendererType
+{
+public static final int TOPIC = 0;
+public static final int TRANSLATEDTOPIC = 1;
+public static final int CONTENTSPECIFICATON = 2;
+}

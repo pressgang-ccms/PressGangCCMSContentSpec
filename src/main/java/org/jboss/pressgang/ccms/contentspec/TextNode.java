de2417e69c9f7c59459e3f3fca9f7abe88968e9f
lnewson <lnewson@redhat.com>
2012-12-06 16:53:33 +1000

    Fixed an issue where the step wasn't calculated for TextNodes.

--- src/main/java/org/jboss/pressgang/ccms/contentspec/TextNode.java
+++ src/main/java/org/jboss/pressgang/ccms/contentspec/TextNode.java
@@ -11,8 +11,35 @@ public class TextNode extends Node {
 
     @Override
     public Integer getStep() {
-        // TODO Auto-generated method stub
-        return null;
+        final Node parent = getParent();
+        if (parent == null)
+            return null;
+        Integer previousNode = 0;
+
+        if (parent instanceof Level) {
+            // Get the position of the level in its parents nodes
+            final Integer nodePos = ((Level) parent).getChildNodes().indexOf(this);
+
+            // If the level isn't the first node then get the previous nodes step
+            if (nodePos > 0) {
+                final Node node = ((Level) parent).getChildNodes().get(nodePos - 1);
+                previousNode = node.getStep();
+                // If the add node is a level then add the number of nodes it contains
+                if (node instanceof Level) {
+                    previousNode = (previousNode == null ? 0 : previousNode) + ((Level) node).getTotalNumberOfChildren();
+                }
+                // The node is the first item so use the parent levels step
+            } else {
+                previousNode = getParent().getStep();
+            }
+            // Make sure the previous nodes step isn't 0
+            previousNode = previousNode == null ? 0 : previousNode;
+
+            // Add one since we got the previous nodes step
+            return previousNode + 1;
+        } else {
+            return null;
+        }
     }
 
     @Override

fdd09bfc74ac54031dd2356fdf8535411ee21085
Katie Miller <katie@codemiller.com>
2013-02-25 09:40:30 +1000

    Added transformer comment test

--- src/test/java/org/jboss/pressgang/ccms/contentspec/utils/CSTransformerCommentTest.java
+++ src/test/java/org/jboss/pressgang/ccms/contentspec/utils/CSTransformerCommentTest.java
@@ -1,5 +1,8 @@
 package org.jboss.pressgang.ccms.contentspec.utils;
 
+import net.sf.ipsedixit.annotation.ArbitraryString;
+import net.sf.ipsedixit.core.StringType;
+import org.jboss.pressgang.ccms.contentspec.Comment;
 import org.jboss.pressgang.ccms.contentspec.wrapper.CSNodeWrapper;
 import org.jboss.pressgang.ccms.utils.constants.CommonConstants;
 import org.junit.Test;
@@ -9,12 +12,14 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.junit.internal.matchers.StringContains.containsString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.contains;
 
 /**
  * @author kamiller@redhat.com (Katie Miller)
  */
 public class CSTransformerCommentTest extends CSTransformerTest {
 
+    @ArbitraryString(type = StringType.ALPHANUMERIC) String text;
     @Mock CSNodeWrapper nodeWrapper;
 
     @Test
@@ -31,5 +36,19 @@ public class CSTransformerCommentTest extends CSTransformerTest {
         } catch (IllegalArgumentException e) {
             assertThat(e.getMessage(), containsString("The passed node is not a Comment"));
         }
+    }
+
+    @Test
+    public void shouldAddTextToComment() throws Exception {
+        // Given a node that is a comment
+        given(nodeWrapper.getNodeType()).willReturn(CommonConstants.CS_NODE_COMMENT);
+        // And that has additional text
+        given(nodeWrapper.getAdditionalText()).willReturn(text);
+
+        // When tranformComment is called
+        Comment result = transformer.transformComment(nodeWrapper);
+
+        // Then the comment should be returned with the text
+        assertThat(result.getText(), containsString(text));
     }
 }

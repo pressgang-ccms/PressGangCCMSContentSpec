6f19e970b45e7d389feb524961c3d06b99bdebce
Lee Newson <lnewson@redhat.com>
2013-05-20 15:13:16 +1000

    Added a couple of basic test to ensure that SpecTopic MetaData is setup correctly.

---
+++ src/test/java/org/jboss/pressgang/ccms/contentspec/ContentSpecTest.java
@@ -0,0 +1,47 @@
+package org.jboss.pressgang.ccms.contentspec;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.jboss.pressgang.ccms.contentspec.enums.TopicType;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContentSpecTest extends BaseUnitTest {
+    private ContentSpec contentSpec;
+
+    @Before
+    public void setUp() {
+        contentSpec = new ContentSpec();
+    }
+
+    @Test
+    public void shouldSetRevisionHistoryWhenAppendingRevisionHistoryMetaData() {
+        // Given a KeyValueNode that is for a spec topic
+        SpecTopic specTopic = new SpecTopic(0, "Revision History");
+        KeyValueNode <SpecTopic> revisionHistory = new KeyValueNode<SpecTopic>("Revision History", specTopic);
+
+        // When appending a child node
+        contentSpec.appendChild(revisionHistory);
+
+        // Then check that the revision history is set
+        assertThat(contentSpec.getRevisionHistory(), is(specTopic));
+        // and the revision history topic type is set
+        assertThat(contentSpec.getRevisionHistory().getTopicType(), is(TopicType.REVISION_HISTORY));
+    }
+
+    @Test
+    public void shouldSetLegalNoticeWhenAppendingLegalNoticeMetaData() {
+        // Given a KeyValueNode that is for a Legal Notice spec topic
+        SpecTopic specTopic = new SpecTopic(0, "Legal Notice");
+        KeyValueNode <SpecTopic> legalNotice = new KeyValueNode<SpecTopic>("Legal Notice", specTopic);
+
+        // When appending a child node
+        contentSpec.appendChild(legalNotice);
+
+        // Then check that the legal notice is set
+        assertThat(contentSpec.getLegalNotice(), is(specTopic));
+        // and the legal notice topic type is set
+        assertThat(contentSpec.getLegalNotice().getTopicType(), is(TopicType.LEGAL_NOTICE));
+    }
+}
